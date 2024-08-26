b3293bb876a134d286d851d32a727c6c4a10b38f
Davide Giliberti <gilo.davide@hotmail.it>
2024-08-26 16:18:49 +0200

    aggiunto biglietto vidimato dao

--- src/main/java/ageria/Application.java
+++ src/main/java/ageria/Application.java
@@ -3,7 +3,6 @@ package ageria;
 import ageria.DAO.AbbonamentoDAO;
 import ageria.DAO.BigliettoDAO;
 import ageria.DAO.PuntodiEmissioneDAO;
-import ageria.entities.PuntodiEmissione;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -11,21 +10,18 @@ import jakarta.persistence.Persistence;
 import java.util.Scanner;
 
 public class Application {
-private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw_atp");
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw_atp");
+
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
         //qui mettiamo i DAO
-        PuntodiEmissioneDAO peD=new PuntodiEmissioneDAO(em);
-        AbbonamentoDAO abbD=new AbbonamentoDAO(em);
-        BigliettoDAO bigD=new BigliettoDAO(em);
+        PuntodiEmissioneDAO peD = new PuntodiEmissioneDAO(em);
+        AbbonamentoDAO abbD = new AbbonamentoDAO(em);
+        BigliettoDAO bigD = new BigliettoDAO(em);
 
 
         //qui cominciamo con lo scanner
-        Scanner scanner=new Scanner(System.in);
-\
-
-
-
+        Scanner scanner = new Scanner(System.in);
 
 
         System.out.println("TUTTO OK!");
---
+++ src/main/java/ageria/DAO/BigliettoVidimatoDAO.java
@@ -0,0 +1,39 @@
+package ageria.DAO;
+
+import ageria.entities.BigliettoVidimato;
+import ageria.exceptions.NotFoundEx;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
+public class BigliettoVidimatoDAO {
+
+    private final EntityManager em;
+
+    public BigliettoVidimatoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(BigliettoVidimato bigliettoVidimato) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(bigliettoVidimato);
+        transaction.commit();
+        System.out.println("il biglietto è stato vidimato correttamente!");
+    }
+
+    public BigliettoVidimato findByID(long id) {
+        BigliettoVidimato found = em.find(BigliettoVidimato.class, id);
+        if (found == null) throw new NotFoundEx(id);
+        return found;
+    }
+
+    public void delete(long id) {
+        BigliettoVidimato found = this.findByID(id);
+        if (found == null) throw new NotFoundEx(id);
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.remove(found);
+        transaction.commit();
+        System.out.println("il biglietto con ID: " + id + " è stato rimosso correttamente");
+    }
+}
