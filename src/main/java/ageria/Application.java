7abc9763a6153240bf03d470017e18dd04dd918c
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-08-26 15:29:16 +0200

    app

--- src/main/java/ageria/Application.java
+++ src/main/java/ageria/Application.java
@@ -1,13 +1,14 @@
 package ageria;
 
-import ageria.DAO.AbbonamentoDAO;
-import ageria.DAO.BigliettoDAO;
-import ageria.DAO.PuntodiEmissioneDAO;
+import ageria.DAO.*;
+import ageria.entities.Mezzo;
 import ageria.entities.PuntodiEmissione;
+import ageria.entities.Utente;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.lang.reflect.Member;
 import java.util.Scanner;
 
 public class Application {
@@ -16,13 +17,18 @@ private static EntityManagerFactory emf = Persistence.createEntityManagerFactory
         EntityManager em = emf.createEntityManager();
         //qui mettiamo i DAO
         PuntodiEmissioneDAO peD=new PuntodiEmissioneDAO(em);
-        AbbonamentoDAO abbD=new AbbonamentoDAO(em);
-        BigliettoDAO bigD=new BigliettoDAO(em);
+        AbbonamentoDAO abbonamentoDAO=new AbbonamentoDAO(em);
+        BigliettoDAO bigliettoDAO=new BigliettoDAO(em);
+        TesseraDAO tesseraDAO=new TesseraDAO(em);
+        TrattaDAO trattaDAO=new TrattaDAO(em);
+        MezzoDAO mezzoDAO=new MezzoDAO(em);
+        UtenteDAO utenteDAO=new UtenteDAO(em);
+
 
 
         //qui cominciamo con lo scanner
         Scanner scanner=new Scanner(System.in);
-\
+
 
 
 
