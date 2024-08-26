e46426f4cb5fd1145ec0336da1f6603b41c86817
ageria20 <ageria20@gmail.com>
2024-08-26 12:20:50 +0200

    DB running correctly

--- src/main/java/ageria/Application.java
+++ src/main/java/ageria/Application.java
@@ -1,8 +1,18 @@
 package ageria;
 
-public class Application {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+public class Application {
+private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw_atp");
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        EntityManager em = emf.createEntityManager();
+
+
+        System.out.println("TUTTO OK!");
+        em.close();
+        emf.close();
     }
 }
